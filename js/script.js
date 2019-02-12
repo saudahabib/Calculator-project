19371416e0730a1b7419668821d13f33701c1df6
saudahabib <saudababs00@gmail.com>
2019-02-12 16:39:27 +0300

    Try out other arithmetic functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var add= function(number1, number2){
-return number1+number2
+var subtract= function(number1, number2){
+return number1-number2
 }
 var number1=parseInt(prompt("enter a number"));
 var number2=parseInt(prompt("enter another number"));
-alert(add(number1, number2));
+alert(subtract(number1, number2));
